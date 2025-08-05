5f2ef2325761cea09bc8aa3723d6e6c52cf5db6e
AmazingAj96 <amazingaj96@gmail.com>
2025-08-04 21:35:30 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,23 @@
-// Replace with your Firebase config
+// Import Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
+
+// Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyACQKI02JmztkNkC8sscx0wqq5Ppjn2oKs",
-  authDomain: "sacredsystemmmo.firebaseapp.com",
-  projectId: "sacredsystemmmo",
-  storageBucket: "sacredsystemmmo.firebasestorage.app",
-  messagingSenderId: "854698379558",
-  appId: "1:854698379558:web:c4ec0830a05bcad854d9e7",
-  databaseURL: "https://sacredsystemmmo-default-rtdb.firebaseio.com"
+  apiKey: "AIzaSyACQKI02JmztkNkC8sscx0wqq5Ppjn2oKs",
+  authDomain: "sacredsystemmmo.firebaseapp.com",
+  databaseURL: "https://sacredsystemmmo-default-rtdb.firebaseio.com",
+  projectId: "sacredsystemmmo",
+  storageBucket: "sacredsystemmmo.firebasestorage.app",
+  messagingSenderId: "854698379558",
+  appId: "1:854698379558:web:c4ec0830a05bcad854d9e7"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Database
+const db = getDatabase(app);
+
+// Export db so dashboard can use it
+export { db };
